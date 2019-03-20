79ed8898d492a8de4bd3113b1afb99af816b2fe9
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-03-20 05:56:25 -0300

    Inclusão DIP

---
+++ Solid/DIP/Contracts/CustomerRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.DIP.Contracts
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public CustomerRepository()
+        {
+
+        }
+
+        public void Save(string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
--- Solid/Program.cs
+++ Solid/Program.cs
@@ -1,4 +1,6 @@
-﻿using Solid.ISP;
+﻿using Solid.DIP;
+using Solid.DIP.Contracts;
+using Solid.ISP;
 using Solid.LSP;
 using Solid.OPC;
 using Solid.SRP;
@@ -12,32 +14,33 @@ namespace Solid
         static void Main(string[] args)
         {
             #region SRP - Simple Responsibility Principle
-            Fornecedor fornecedor = new Fornecedor();
-            fornecedor.ChangeName("Ricardo");
+            //Fornecedor fornecedor = new Fornecedor();
+            //fornecedor.ChangeName("Ricardo");
 
-            IFornecedorRepository rep = new FornecedorRepository();
-            rep.Salvar(fornecedor);
+            //IFornecedorRepository rep = new FornecedorRepository();
+            //rep.Salvar(fornecedor);
             #endregion
 
             #region OCP - Open/Closed Principle
-            var p = new Pessoa("Ricardo");
-            p.ChangeName("Ricardo Silva");
+            //var p = new Pessoa("Ricardo");
+            //p.ChangeName("Ricardo Silva");
 
-            var pf = new PessoaFisica("Ricardo", "999.999.999.-99");
+            //var pf = new PessoaFisica("Ricardo", "999.999.999.-99");
             #endregion
 
             #region LSP - Liskov Substituition Principle
-            var veiculo = new Carro();
-            veiculo.LigarMotor();
+            //var veiculo = new Carro();
+            //veiculo.LigarMotor();
             #endregion
 
             #region ISP - Interface Segregation Principle
-            NotaFiscal nf = new NotaFiscal();
-            nf.RealizarPedido();
+            //NotaFiscal nf = new NotaFiscal();
+            //nf.RealizarPedido();
             #endregion
 
             #region DIP - Dependency Inversion Principle
-
+            //var rep = new CustomerRepository();
+            //var customerService = new CustomerService(rep);
             #endregion
 
             Console.ReadKey();
